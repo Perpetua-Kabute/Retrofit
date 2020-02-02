ead6481f5cdda8252c8c43ba0a468a72cdd52765
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-02-02 12:08:24 +0300

    Added UI

---
+++ app/src/main/java/com/androidplayground/retrofit/activity/MainActivity.java
@@ -0,0 +1,54 @@
+package com.androidplayground.retrofit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.androidplayground.retrofit.R;
+import com.androidplayground.retrofit.model.Movie;
+import com.androidplayground.retrofit.model.MovieResponse;
+import com.androidplayground.retrofit.rest.ApiClient;
+import com.androidplayground.retrofit.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    private final static String API_KEY = "920732fb5a8ec81a588133169902c1e3";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse>call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                Log.d(TAG, "Number of movies received: " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse>call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+}
