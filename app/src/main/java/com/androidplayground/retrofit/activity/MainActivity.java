2fd8c964b7439f705fac51b32514cbb09a4aff0d
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-02-02 15:13:30 +0300

    Display the movies

--- app/src/main/java/com/androidplayground/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/androidplayground/retrofit/activity/MainActivity.java
@@ -2,10 +2,13 @@ package com.androidplayground.retrofit.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.androidplayground.retrofit.R;
+import com.androidplayground.retrofit.adapter.MoviesAdapter;
 import com.androidplayground.retrofit.model.Movie;
 import com.androidplayground.retrofit.model.MoviesResponse;
 import com.androidplayground.retrofit.rest.ApiClient;
@@ -33,15 +36,19 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
         Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
         call.enqueue(new Callback<MoviesResponse>() {
             @Override
-            public void onResponse(Call<MoviesResponse>call, Response<MoviesResponse> response) {
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                int statusCode = response.code();
                 List<Movie> movies = response.body().getResults();
-                Log.d(TAG, "Number of movies received: " + movies.size());
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
             }
 
             @Override
